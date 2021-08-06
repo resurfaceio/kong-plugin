1cf580a495e96f01f843f93d6a9ffcc1fa7cd9f1
Ramón Márquez <ramn.marquez@gmail.com>
2021-08-06 11:43:11 -0400

    Update schema.lua

--- lua/schema.lua
+++ lua/schema.lua
@@ -2,7 +2,7 @@ local typedefs = require "kong.db.schema.typedefs"
 local url = require "socket.url"
 
 return {
-  name = "myplugin-hlog",
+  name = "resurfaceio-logger",
   fields = {
     { protocols = typedefs.protocols },
     { config = {
